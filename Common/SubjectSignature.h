4c5c9d3c08ee0ec445babb80e912ab939b273aae
classfly <fujianbo08@126.com>
2014-10-20 20:55:57 +0800

    Create SubjectSignature.h

---
+++ Common/SubjectSignature.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <OgreString.h>
+namespace hiveCrowd
+{
+	namespace Common
+	{
+		const Ogre::String KERNEL_EVENT_SIG_REMOVE_OBJECT = "REMOVE_OBJECT";
+		const Ogre::String KERNEL_EVENT_SIG_POSITION_UPDATE = "POSITION_UPDATE";
+		const Ogre::String KERNEL_EVENT_SIG_SCENE_UPDATE = "SCENEGROUP_UPDATE";
+		const Ogre::String KERNEL_OBSERVER_SCENE_INFO_MODULE_GROUP = "SCENE_INFO_MODULE_GROUP";
+	}
+}
