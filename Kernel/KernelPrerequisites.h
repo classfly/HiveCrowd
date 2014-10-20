64f4589bf60d731c1ef25cb6b765c7f77d958b05
classfly <fujianbo08@126.com>
2014-10-20 20:59:47 +0800

    Create KernelPrerequisites.h

---
+++ Kernel/KernelPrerequisites.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <OgrePrerequisites.h>
+namespace hiveCrowd
+{
+	namespace Kernel
+	{
+		class CAgent;
+		class CSceneEntity;
+
+		typedef Ogre::SharedPtr<CAgent>		  AgentPtr;
+		typedef Ogre::SharedPtr<CSceneEntity> SceneEntityPtr;
+	}
+}
